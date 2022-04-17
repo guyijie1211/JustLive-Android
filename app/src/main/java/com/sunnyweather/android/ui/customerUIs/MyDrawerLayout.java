e5968ed15e0ea1ed3bc6abfdfad2fb4c4303b193
YJ1211 <1056025931@qq.com>
2022-04-17 10:02:31 +0800

    0.2.8

---
+++ app/src/main/java/com/sunnyweather/android/ui/customerUIs/MyDrawerLayout.java
@@ -0,0 +1,33 @@
+package com.sunnyweather.android.ui.customerUIs;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.blankj.utilcode.util.ScreenUtils;
+
+public class MyDrawerLayout extends DrawerLayout {
+
+    public MyDrawerLayout(Context context) {
+        super(context);
+    }
+
+    public MyDrawerLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyDrawerLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        widthMeasureSpec = MeasureSpec.makeMeasureSpec(
+                MeasureSpec.getSize(widthMeasureSpec), MeasureSpec.EXACTLY);
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(
+                MeasureSpec.getSize(heightMeasureSpec), MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+}
